2f827ac344e55057758e604bc4a8316fc8ee7f57
shubhamyadav20 <shubhamyadav.sy54@gmail.com>
2019-09-12 22:09:12 -0700

    Not Passing Two Number

---
+++ TddCalApp.Test/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TddCalApp.Library;
+
+namespace TddCalApp.Test
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+        public void ShouldReturnPositiveonPassingTwoPositiveNumber()
+        {
+            Calculator TestCalculator = new Calculator();
+            int num1 = 2;
+            int num2 = 3;
+           int result = TestCalculator.Add(num1, num2);
+            Assert.AreEqual(5, result, "Testing  two Integer 3 and 2");
+
+        }
+    }
+}
